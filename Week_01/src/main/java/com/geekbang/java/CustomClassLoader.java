f3576f4df29d7985b032bcaccae890ef3c43df1f
wenhai <418480642@qq.com>
2020-10-21 21:33:47 +0800

    第一周作业

---
+++ Week_01/src/main/java/com/geekbang/java/CustomClassLoader.java
@@ -0,0 +1,38 @@
+package com.geekbang.java;
+
+
+import java.io.*;
+
+/**
+ * 自定义类加载器
+ *
+ * @author wenhai
+ * @date 2020/10/21
+ */
+public class CustomClassLoader extends ClassLoader {
+
+    @Override
+    protected Class<?> findClass(String name) throws ClassNotFoundException {
+        String filePath = System.getProperty("user.dir") + "/Week_01/src/main/Resources/META-INF/Hello.xlass";
+        try (InputStream in = new FileInputStream(filePath)) {
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            byte[] buffer = new byte[1024 * 4];
+            int n;
+            while ((n = in.read(buffer)) != -1) {
+                out.write(buffer, 0, n);
+            }
+            byte[] bytes = out.toByteArray();
+            this.decode(bytes);
+            return defineClass(name, bytes, 0, bytes.length);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private void decode(byte[] bytes) {
+        for (int i = 0; i < bytes.length; i++) {
+            bytes[i] = (byte) (255 - bytes[i]);
+        }
+    }
+}
---
+++ Week_01/src/main/java/com/geekbang/java/CustomClassLoaderDemo.java
@@ -0,0 +1,23 @@
+package com.geekbang.java;
+
+import java.lang.reflect.Method;
+
+/**
+ * 自定义类加载实例
+ *
+ * @author wenhai
+ * @date   2020/10/21
+ */
+public class CustomClassLoaderDemo {
+    public static void main(String[] args) throws Exception {
+        Class<?> helloClass = new CustomClassLoader().loadClass("Hello");
+        Method helloMethod = helloClass.getDeclaredMethod("hello");
+        Object hello = helloClass.newInstance();
+        helloMethod.invoke(hello);
+
+    }
+
+
+
+
+}
